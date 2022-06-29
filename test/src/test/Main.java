8d87c67330258e4b86792a36c80575e58fe5bb42
Administrator <Administrator@SC-202106161522>
2022-06-29 10:24:38 +0900

    연습중 2222

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// push 연습
 		
-		System.out.println("Push Push");
-		System.out.println("baby");
+		System.out.println("잘되고 있는거");
+		System.out.println("맞겠지 ?");
 	}
 
 }
